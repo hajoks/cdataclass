fe3b35bd44392746842228487ad01cf46a133dc8
hajoks <syari4369@gmail.com>
2023-03-12 00:25:25 +0900

    v0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="cdataclass",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(exclude=("tests*",)),
     package_data={"cdataclass": ["py.typed"]},
     author="hajoks",
